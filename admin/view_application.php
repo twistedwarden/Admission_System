a2f9a7c66545cb29fa110387bb5332902822fea2
Monsai <149811293+twistedwarden@users.noreply.github.com>
2025-05-05 01:21:02 +0800

    Update view_application.php

--- admin/view_application.php
+++ admin/view_application.php
@@ -147,12 +147,6 @@ require_once 'admin_header.php';
                 </svg>
                 Edit Application
             </a>
-            <a href="print_application.php?id=<?= $applicationId ?>" target="_blank" class="btn-primary flex items-center">
-                <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"></path>
-                </svg>
-                Print
-            </a>
         </div>
     </div>
     
@@ -290,7 +284,10 @@ require_once 'admin_header.php';
                     <h3 class="text-gray-500 font-medium mb-2">Program Code</h3>
                     <p class="text-lg font-semibold"><?= htmlspecialchars($program['code']) ?></p>
                 </div>
-                
+                <div class="mb-4">
+                    <h3 class="text-gray-500 font-medium mb-2">Year Level</h3>
+                    <p class="text-lg font-semibold"><?= htmlspecialchars($application['year_level']) ?></p>
+                </div>
                 <div class="mb-4">
                     <h3 class="text-gray-500 font-medium mb-2">Program Name</h3>
                     <p class="text-lg font-semibold"><?= htmlspecialchars($program['name']) ?></p>
